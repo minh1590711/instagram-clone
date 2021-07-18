8ccd8a00e02ee843970b5fb0fb851fbd6abb6336
minh1590711 <minh1590711@gmail.com>
2021-07-18 16:43:01 +0700

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-9/64294735_2802464389795813_4365494283952717824_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=C8cBYJ2Vnx8AX-m7KyI&tn=WygDx5sU5LtIdgJC&_nc_ht=scontent-sin6-1.xx&oh=9e6adc9c1fac14021c99e682559fbd12&oe=60F97D8D" style="height: 220px" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>Minh Lê</h1>
+            </div>
+            <div class="d-flex">
+                <div class="p-3"><strong>200</strong> Bài đăng</div>
+                <div class="p-3"><strong>1k</strong> Followers</div>
+                <div class="p-3">Đang theo dõi <strong>200</strong> </div>
+            </div>
+            <div class="pt-4 font-weight-bold">First Clone Project</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            <div>
+                <a href="https://www.lipsum.com/">Non-profit Project</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://dummyimage.com/600x400/1b016e/fff.jpg&text=Dummy+Image" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://dummyimage.com/600x400/1b016e/fff.jpg&text=Dummy+Image" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://dummyimage.com/600x400/1b016e/fff.jpg&text=Dummy+Image" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
